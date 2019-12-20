e580c0ca3a4f0872f769e8e365b08c36234174c2
ssongman <ssongmantop@gmail.com>
2019-12-20 13:24:10 +0000

    express server

---
+++ react/management/client/build/precache-manifest.308cc002fcd27835b0c7ece9518b67db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6215719204d81d28c4679851e77631",
+    "url": "/index.html"
+  },
+  {
+    "revision": "014b3d18621c2ae69766",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9409c66e4b068facdb4b",
+    "url": "/static/js/2.7c8417ec.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.7c8417ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "014b3d18621c2ae69766",
+    "url": "/static/js/main.a7b0481d.chunk.js"
+  },
+  {
+    "revision": "b3a06c42a35e0ce0ab15",
+    "url": "/static/js/runtime-main.627dd1b7.js"
+  }
+]);
