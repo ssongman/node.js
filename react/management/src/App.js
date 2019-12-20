bb51fa2fe69a1b921dba7d60a93197142c89b5e6
ssongman <ssongmantop@gmail.com>
2019-12-20 18:48:09 +0900

    Update App.js

--- react/management/src/App.js
+++ react/management/src/App.js
@@ -30,36 +30,29 @@ const customer = [
 ]
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-      <Customer
-        id      ={customer[0].id}
-        image   ={customer[0].image}
-        name    ={customer[0].name}
-        birthday={customer[0].birthday}
-        gender  ={customer[0].gender}
-        job     ={customer[0].job}
-      />
-      <Customer
-        id      ={customer[1].id}
-        image   ={customer[1].image}
-        name    ={customer[1].name}
-        birthday={customer[1].birthday}
-        gender  ={customer[1].gender}
-        job     ={customer[1].job}
-      />
-      <Customer
-        id      ={customer[2].id}
-        image   ={customer[2].image}
-        name    ={customer[2].name}
-        birthday={customer[2].birthday}
-        gender  ={customer[2].gender}
-        job     ={customer[2].job}
-      />
-      </div>
-    );
-  }
+render() {
+return (
+<div>
+<Table>
+<TableHead>
+<TableRow>
+<TableCell>번호</TableCell>
+<TableCell>이미지</TableCell>
+<TableCell>이름</TableCell>
+<TableCell>생년월일</TableCell>
+<TableCell>성별</TableCell>
+<TableCell>직업</TableCell>
+</TableRow>
+</TableHead>
+<TableBody>
+{customers.map(c => {
+return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+})}
+</TableBody>
+</Table>
+</div>
+);
+}
 }
 
 export default App;
