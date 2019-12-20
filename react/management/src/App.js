390f8c308b4ff4c284d4c2153abd6650d780c098
ssongman <ssongmantop@gmail.com>
2019-12-20 18:36:48 +0900

    Update App.js

--- react/management/src/App.js
+++ react/management/src/App.js
@@ -33,9 +33,30 @@ class App extends Component {
   render() {
     return (
       <div>
-        {customers.map((c) => {
-          return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-        })}
+      <Customer
+        id      ={customer[0].id}
+        image   ={customer[0].image}
+        name    ={customer[0].name}
+        birthday={customer[0].birthday}
+        gender  ={customer[0].gender}
+        job     ={customer[0].job}
+      />
+      <Customer
+        id      ={customer[1].id}
+        image   ={customer[1].image}
+        name    ={customer[1].name}
+        birthday={customer[1].birthday}
+        gender  ={customer[1].gender}
+        job     ={customer[1].job}
+      />
+      <Customer
+        id      ={customer[2].id}
+        image   ={customer[2].image}
+        name    ={customer[2].name}
+        birthday={customer[2].birthday}
+        gender  ={customer[2].gender}
+        job     ={customer[2].job}
+      />
       </div>
     );
   }
