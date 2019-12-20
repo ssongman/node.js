4465f908b1183f82cb4f9b4dedb285d8b0df072c
ssongman <ssongmantop@gmail.com>
2019-12-20 05:13:01 +0000

    react

---
+++ react/management/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Customer from './components/Customer'
+import './App.css';
+
+const customer = {
+  'name': 'Mr. Hong',
+  'birthday': '961222',
+  'gender': 'Man',
+  'job': 'High School'
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Customer
+        name={customer.name}
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      />
+    );
+  }
+}
+
+export default App;
